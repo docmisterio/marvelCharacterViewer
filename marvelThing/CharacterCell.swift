aa744f12fb583e2a8a4e20e1ab2d921ba35d9b85
Zach Becker <doc.misterio@gmail.com>
2020-12-17 11:37:25 -0600

    Created a file for the custom cell and initiliazed it

---
+++ marvelThing/CharacterCell.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+class CharacterCell: UITableViewCell {
+    let characterPhoto = UIImageView()
+    let characterName = UILabel()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: .default, reuseIdentifier: reuseIdentifier)
+    
+        
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    
+    
+    
+
+}
--- marvelThing/MarvelData.swift
+++ marvelThing/MarvelData.swift
@@ -2,4 +2,10 @@ import Foundation
 
 struct Character: Codable {
     var name: String
+    var thumbnail: CharacterPhoto
+}
+
+struct CharacterPhoto: Codable {
+    var path: String
+    var `extension`: String
 }
